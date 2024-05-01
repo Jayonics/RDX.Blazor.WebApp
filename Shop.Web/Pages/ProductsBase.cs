8ace19cc7e32ee7b4fbe7b13dd5643e9126bf587
James.Hopwood <james.hopwood@influentialsoftware.com>
2024-05-01 17:25:05 +0100

    Initial code for Blazor frontend.

---
+++ Shop.Web/Pages/ProductsBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using Shop.Models.Dtos;
+using Shop.Web.Services.Contracts;
+
+namespace Shop.Web.Pages
+{
+    public class ProductsBase : ComponentBase
+    {
+        [Inject]
+        public IProductService ProductService { get; set; }
+
+        public IEnumerable<ProductDto> Products { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Products = await ProductService.GetItems();
+        }
+    }
+}
---
+++ Shop.Web/Services/Contracts/IProductService.cs
@@ -0,0 +1,9 @@
+﻿using Shop.Models.Dtos;
+
+namespace Shop.Web.Services.Contracts
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDto>> GetItems();
+    }
+}
