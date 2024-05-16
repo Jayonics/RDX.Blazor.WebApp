807749d9d39c430f99edbad869604c783fe96c63
James.Hopwood <james.hopwood@influentialsoftware.com>
2024-05-16 10:12:54 +0100

    Added Roles to Identity database

--- Shop.WebApp/Data/ApplicationDbContext.cs
+++ Shop.WebApp/Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,5 +6,30 @@ namespace Shop.WebApp.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Seed Roles
+            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Id = "1",
+                Name = "Admin",
+                NormalizedName = "Admin"
+            });
+            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Id = "2",
+                Name = "Staff",
+                NormalizedName = "Staff"
+            });
+            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Id = "3",
+                Name = "Customer",
+                NormalizedName = "Customer"
+            });
+        }
     }
 }
