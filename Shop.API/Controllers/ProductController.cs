e22c308932ac2356884bb55724309a8bb04d7fb7
James.Hopwood <james.hopwood@influentialsoftware.com>
2024-05-28 11:38:10 +0100

    Rename references to private instance of productRepository to _productRepository

--- Shop.API/Controllers/ProductController.cs
+++ Shop.API/Controllers/ProductController.cs
@@ -1,11 +1,9 @@
-﻿using Azure.Storage.Blobs;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Shop.API.Extensions;
 using Shop.API.Repositories.Contracts;
 using Shop.Models.Dtos;
 using Shop.Models.Requests;
-using System.Text.Json;
 
 namespace Shop.API.Controllers
 {
@@ -17,47 +15,34 @@ namespace Shop.API.Controllers
     public class ProductController : ControllerBase
     {
         readonly ILogger _logger;
-        readonly IProductRepository productRepository;
-        // Create the private instance field for the blob service client
-        private readonly BlobServiceClient _blobServiceClient;
+        readonly IProductRepository _productRepository;
+        private readonly IConfiguration _configuration;
 
         /// <summary>
         ///     Initializes a new instance of the <see cref="ProductController" /> class.
         /// </summary>
         /// <param name="productRepository">The product repository.</param>
-        /// <param name="blobServiceClient"></param>
-        public ProductController(IProductRepository productRepository, BlobServiceClient blobServiceClient, ILogger<ProductController> logger)
+        public ProductController(IProductRepository productRepository, ILogger<ProductController> logger, IConfiguration configuration)
         {
-            this.productRepository = productRepository;
+            _productRepository = productRepository;
             _logger = logger;
-            _blobServiceClient = blobServiceClient;
+            _configuration = configuration;
         }
 
         /// <summary>
         ///     Gets all items from the product repository.
         /// </summary>
         /// <returns>A list of product DTOs.</returns>
-        [HttpGet]
+        [HttpGet()]
         public async Task<ActionResult<IEnumerable<ProductDto>>> GetProducts()
         {
             // Try to execute the following code
             try
             {
-                // Get the container "test" within the blob service
-                var containerClient = _blobServiceClient.GetBlobContainerClient("test").GetBlobsAsync();
-                // Output all items in the blob container
-                await foreach (var item in containerClient)
-                {
-                    // Log each property of the object to the console
-                    _logger.LogDebug(JsonSerializer.Serialize(item, new JsonSerializerOptions { WriteIndented = true }));
-                }
-
-
-
                 // Fetch all products from the repository
-                var products = await productRepository.GetProducts();
+                var products = await _productRepository.GetProducts();
                 // Fetch all product categories from the repository
-                var productCategories = await productRepository.GetCategories();
+                var productCategories = await _productRepository.GetCategories();
 
                 // If either products or product categories are null, return a NotFound status
                 if (products == null || productCategories == null)
@@ -81,10 +66,10 @@ namespace Shop.API.Controllers
         [HttpGet("{id:int}")]
         public async Task<ActionResult<ProductDto>> GetProduct([FromRoute]int id)
         {
-            var product = await productRepository.GetProduct(id);
+            var product = await _productRepository.GetProduct(id);
             if (product == null) return NotFound();
 
-            var productCategory = await productRepository.GetCategory(product.CategoryId);
+            var productCategory = await _productRepository.GetCategory(product.CategoryId);
             if (productCategory == null) return NotFound();
 
             var productDto = product.ConvertToDto(productCategory);
@@ -100,7 +85,7 @@ namespace Shop.API.Controllers
 
             try
             {
-                product = await productRepository.UpdateProduct(product);
+                product = await _productRepository.UpdateProduct(product);
             }
             catch (DbUpdateConcurrencyException)
             {
@@ -109,7 +94,7 @@ namespace Shop.API.Controllers
                 throw;
             }
 
-            var productCategory = await productRepository.GetCategory(product.CategoryId);
+            var productCategory = await _productRepository.GetCategory(product.CategoryId);
             if (productCategory == null) return NotFound();
 
             var updatedProductDto = product.ConvertToDto(productCategory);
@@ -118,19 +103,19 @@ namespace Shop.API.Controllers
 
         async Task<bool> ProductExists([FromRoute]int id)
         {
-            var product = await productRepository.GetProduct(id);
+            var product = await _productRepository.GetProduct(id);
             return product != null;
         }
 
         [HttpDelete("{id:int}")]
         public async Task<ActionResult> DeleteProduct([FromRoute]int id)
         {
-            var product = await productRepository.GetProduct(id);
+            var product = await _productRepository.GetProduct(id);
             if (product == null) return NotFound();
 
             try
             {
-                await productRepository.DeleteProduct(id);
+                await _productRepository.DeleteProduct(id);
                 return Ok();
             }
             catch (Exception)
@@ -157,9 +142,9 @@ namespace Shop.API.Controllers
             if (productDto.ConvertToEntity() != null)
                 try
                 {
-                    var product = await productRepository.AddProduct(productDto.ConvertToEntity());
-                    var productCategory = await productRepository.GetCategory(product.CategoryId);
                     return Ok(product.ConvertToDto(productCategory));
+                    var product = await _productRepository.AddProduct(productDto.ConvertToEntity());
+                    var productCategory = await _productRepository.GetCategory(product.CategoryId);
 
                 }
                 catch (Exception exception)
