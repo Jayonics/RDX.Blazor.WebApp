032c8d660b3de31d3b5f410d1095e060cb811829
James.Hopwood <james.hopwood@influentialsoftware.com>
2024-06-11 11:21:35 +0100

    User Story #47 - Add audit logging Custom Serilog Logger extension method for logging information about the invoking class and method.

---
+++ Shop.WebApp/Extensions/LoggerExtensions.cs
@@ -0,0 +1,16 @@
+using System.Runtime.CompilerServices;
+using ILogger = Serilog.ILogger;
+
+namespace Shop.WebApp.Extensions
+{
+    public static class LoggerExtensions
+    {
+        public static ILogger WithClassAndMethodNames<T>(this ILogger logger,
+            [CallerMemberName] string memberName = "")
+        {
+            var className = typeof(T).Name;
+            return logger.ForContext("ClassName", className)
+                .ForContext("MethodName", memberName);
+        }
+    }
+}
